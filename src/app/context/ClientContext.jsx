bc7594f9ca69965563daa51ca3d8eee43329cc4b
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2024-03-19 08:06:26 -0300

    aaveces gano aveces no

--- src/app/context/ClientContext.jsx
+++ src/app/context/ClientContext.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { createContext, useState, useEffect } from "react";
+import { createContext, useContext ,useState, useEffect } from "react";
 import { UserAuth } from "./AuthContext";
 import { clienteExiste } from "../firebase";
 
