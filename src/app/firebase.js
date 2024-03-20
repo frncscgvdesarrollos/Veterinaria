3d90f5f0e90436b0b6e167771c93b82036cb9918
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2024-03-20 10:13:16 -0300

    variables de entorno arreglando problema

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -5,13 +5,13 @@ import { getStorage , ref,  uploadBytes, getDownloadURL, getBytes  } from "fireb
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnY6y3nq7zXbqTq2hNn7VwN7H1qWl6Hv0",
-  authDomain: "veterinaria-b1ce8.firebaseapp.com",
-  projectId: "veterinaria-b1ce8",
-  storageBucket: "veterinaria-b1ce8.appspot.com",
-  messagingSenderId: "773108259305",
-  appId: "1:773108259305:web:4d70eab6257c44b3ca5378",
-  measurementId: "G-V7582EQPKP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
