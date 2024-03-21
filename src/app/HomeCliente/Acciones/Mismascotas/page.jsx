9d260bf6e9a012cf27d218a9d8ec3f99207b6e8d
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2024-03-21 00:21:20 -0300

    arreglos de css

--- src/app/HomeCliente/Acciones/Mismascotas/page.jsx
+++ src/app/HomeCliente/Acciones/Mismascotas/page.jsx
@@ -8,8 +8,8 @@ export default function MisMascotas() {
 
   return (
     <div className="flex flex-col justify-center items-center HomeCliente p-4 rounded-lg  border-gray-800">
-      <h2 className="text-3xl font-bold text-gray-800 ml-auto mr-24">Tus Mascotas</h2>
-      <div className="flex flex-row flex-wrap justify-center space-x-4 mb-4 ml-12 sm:mr-auto text-white text-xl">
+      <h2 className="text-3xl font-bold text-gray-800 mr-auto mb-4 ">Tus Mascotas</h2>
+      <div className="flex  mr-auto mb-4">
         <button onClick={() => setNuevaMascota(!nuevaMascota)} className="btn btn-primary m-3 bg-blue-500 rounded border-2-black p-2">Agregar mascota</button>
         <button className="btn btn-primary m-3 bg-green-700 rounded border-2-black p-2 ">Editar Mascota</button>    
         {/* <button className="btn btn-primary m-3 bg-pink-500 rounded border-2-black p-2">Cruzar Mascota</button> */}
