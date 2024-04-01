a07805892d86fc6844d274e6f41380e40492b11c
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2024-04-01 16:32:11 -0300

    mercadopagoProgando

--- src/app/newClient/datosCliente/page.jsx
+++ src/app/newClient/datosCliente/page.jsx
@@ -3,13 +3,15 @@ import { useEffect, useState } from 'react';
 import FormCliente from '../../components/FormCliente';
 import { clienteExisteConTerminosTRUE } from '../../firebase';
 import { UserAuth } from '../../context/AuthContext';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation'; // Importar useRouter en lugar de redirect
 
 export default function DatosCliente() {
   const { user } = UserAuth();
   const uid = user?.uid;
   const [terminosAceptados, setTerminosAceptados] = useState(false);
   const [errorVerificacion, setErrorVerificacion] = useState(null);
+  const router = useRouter(); // Crear una instancia del enrutador
+
   useEffect(() => {
     if (user) {
       clienteExisteConTerminosTRUE(uid)
@@ -23,20 +25,23 @@ export default function DatosCliente() {
         })
         .catch((error) => {
           console.error("Error verifying terms:", error);
-          setErrorVerificacion(error); // Keep for generic error display
+          setErrorVerificacion(error);
+          // Keep for generic error display
         });
     }
   }, [user, uid]); // Agregar 'uid' a la lista de dependencias
 
   useEffect(() => {
     if (terminosAceptados) {
-      redirect('/HomeCliente');
+      router.push('/HomeCliente'); // Usar router.push en lugar de redirect
     }
-  }, [terminosAceptados]);
+  }, [terminosAceptados, router]); // Agregar router a la lista de dependencias
 
   return (
     <div className='bg-gray-800 h-auto p-5'>
-      <h1 className='text-4xl text-center font-bold mt-5 mb-5 text-red-400 underline'>Informacion personal</h1>
+      <h1 className='text-4xl text-center font-bold mt-5 mb-5 text-red-400 underline'>
+        Informacion personal
+      </h1>
       {errorVerificacion && (
         <p className='text-red-500 text-center mb-5'>
           Error al verificar la aceptación de términos: {errorVerificacion.message}
