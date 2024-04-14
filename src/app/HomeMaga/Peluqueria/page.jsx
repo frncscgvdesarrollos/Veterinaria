cd2e5bc22d599ad385cad22655c5d219389845fe
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2024-04-14 10:48:55 -0300

    cambios CSS

--- src/app/HomeMaga/Peluqueria/page.jsx
+++ src/app/HomeMaga/Peluqueria/page.jsx
@@ -1,4 +1,4 @@
-'use client';
+'use client'
 import { useState, useEffect } from 'react';
 import { getTurnosPeluqueria, avanzarEstadoTurno, updateCanil } from '../../firebase';
 import Image from 'next/image';
@@ -82,49 +82,107 @@ export default function Peluqueria() {
                         </tr>
                     </thead>
                     <tbody className="bg-white divide-y divide-gray-200">
-    {turnos.map((turno, index) => (
-        turno.estadoDelTurno !== "confirmar" &&
-        turno.estadoDelTurno !== "finalizado" &&
-        turno.estadoDelTurno !== "cancelado" && (
-            <tr key={index} className={index % 2 === 0 ? 'bg-violet-100' : 'bg-cyan-100'}>
-                <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{turno.selectedPet}</td>
-                <td className='px-6 py-4 whitespace-nowrap'>
-                    <Image src={turno.foto} alt={turno.selectedImg} width={50} height={50} className='rounded-full' />
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">{turno.selectedServicio}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{turno.info === "Agrega cualquier informacion que quieras dejar aclarada" ? <span>no hay info</span> : turno.info}</td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                    <select
-                        value={turno.canilPeluqueria}
-                        onChange={(e) => handleUpdateCanil(turno.id, e.target.value)}
-                    >
-                        {["1", " 2", "3", "4", "5", "6", "7"].map((canilNumber) => (
-                            <option key={canilNumber} value={canilNumber}>
-                                Canil {canilNumber}
-                            </option>
+                        {turnos.map((turno, index) => (
+                            turno.estadoDelTurno !== "confirmar" &&
+                            turno.estadoDelTurno !== "finalizado" &&
+                            turno.estadoDelTurno !== "cancelado" &&
+                            turno.selectedTurno !== "tarde" && (
+                                <tr key={index} className={index % 2 === 0 ? 'bg-violet-100' : 'bg-cyan-100'}>
+                                    <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{turno.selectedPet}</td>
+                                    <td className='px-6 py-4 whitespace-nowrap'>
+                                        <Image src={turno.foto} alt={turno.selectedImg} width={50} height={50} className='rounded-full' />
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{turno.selectedServicio}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{turno.info === "Agrega cualquier informacion que quieras dejar aclarada" ? <span>no hay info</span> : turno.info}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                        <select
+                                            value={turno.canilPeluqueria}
+                                            onChange={(e) => handleUpdateCanil(turno.id, e.target.value)}
+                                        >
+                                            {["1", " 2", "3", "4", "5", "6", "7"].map((canilNumber) => (
+                                                <option key={canilNumber} value={canilNumber}>
+                                                    Canil {canilNumber}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </td>
+                                    <td className='px-6 py-4 whitespace-nowrap'>{turno.estadoDelTurno}</td>
+                                    <td className='flex justify-center'>
+                                        {turno.estadoDelTurno === "veterinaria" &&
+                                            <button onClick={() => handleEstadoUpdate(turno.id, turno.estadoDelTurno)} className='bg-red-500 p-2 m-2 text-white'>En proceso</button>
+                                        }
+                                        {turno.estadoDelTurno === "proceso" &&
+                                            <button onClick={() => handleEstadoUpdate(turno.id, turno.estadoDelTurno)} className='bg-red-500 p-2 m-2 text-white'>Terminado</button>
+                                        }
+                                        {turno.estadoDelTurno !== "veterinaria" && turno.estadoDelTurno !== "proceso" &&
+                                            <p className='bg-red-500 p-2 m-2 text-white'>EN ESPERA</p>
+                                        }
+                                    </td>
+                                </tr>
+                            )
                         ))}
-                    </select>
-                </td>
-                <td className='px-6 py-4 whitespace-nowrap'>{turno.estadoDelTurno}</td>
-                <td className='flex justify-center'>
-                    {turno.estadoDelTurno === "veterinaria" &&
-                        <button onClick={() => handleEstadoUpdate(turno.id, turno.estadoDelTurno)} className='bg-red-500 p-2 m-2 text-white'>En proceso</button>
-                    }
-                    {turno.estadoDelTurno === "proceso" &&
-                        <button onClick={() => handleEstadoUpdate(turno.id, turno.estadoDelTurno)} className='bg-red-500 p-2 m-2 text-white'>Terminado</button>
-                    }
-                    {turno.estadoDelTurno !== "veterinaria" && turno.estadoDelTurno !== "proceso" &&
-                        <p className='bg-red-500 p-2 m-2 text-white'>EN ESPERA</p>
-                    }
-                </td>
-            </tr>
-        )
-    ))}
-</tbody>
+                    </tbody>
+                </table>
+            </div>
+            <div className="overflow-x-auto mt-6">
+                <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-50">
+                        <tr>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Turno</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Mascota</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Foto</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Servicio</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Info</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Canil</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Estado</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Proximo estado</th>
+                        </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                        {turnos.map((turno, index) => (
+                            turno.estadoDelTurno !== "confirmar" &&
+                            turno.estadoDelTurno !== "finalizado" &&
+                            turno.estadoDelTurno !== "cancelado" &&
+                            turno.selectedTurno !== "mañana" && (
+                                <tr key={index} className={index % 2 === 0 ? 'bg-violet-100' : 'bg-cyan-100'}>
+                                    <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{turno.selectedPet}</td>
+                                    <td className='px-6 py-4 whitespace-nowrap'>
+                                        <Image src={turno.foto} alt={turno.selectedImg} width={50} height={50} className='rounded-full' />
+                                    </td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{turno.selectedServicio}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{turno.info === "Agrega cualquier informacion que quieras dejar aclarada" ? <span>no hay info</span> : turno.info}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">
+                                        <select
+                                            value={turno.canilPeluqueria}
+                                            onChange={(e) => handleUpdateCanil(turno.id, e.target.value)}
+                                        >
+                                            {["1", " 2", "3", "4", "5", "6", "7"].map((canilNumber) => (
+                                                <option key={canilNumber} value={canilNumber}>
+                                                    Canil {canilNumber}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </td>
+                                    <td className='px-6 py-4 whitespace-nowrap'>{turno.estadoDelTurno}</td>
+                                    <td className='flex justify-center'>
+                                        {turno.estadoDelTurno === "veterinaria" &&
+                                            <button onClick={() => handleEstadoUpdate(turno.id, turno.estadoDelTurno)} className='bg-red-500 p-2 m-2 text-white'>En proceso</button>
+                                        }
+                                        {turno.estadoDelTurno === "proceso" &&
+                                            <button onClick={() => handleEstadoUpdate(turno.id, turno.estadoDelTurno)} className='bg-red-500 p-2 m-2 text-white'>Terminado</button>
+                                        }
+                                        {turno.estadoDelTurno !== "veterinaria" && turno.estadoDelTurno !== "proceso" &&
+                                            <p className='bg-red-500 p-2 m-2 text-white'>EN ESPERA</p>
+                                        }
+                                    </td>
+                                </tr>
+                            )
+                        ))}
+                    </tbody>
                 </table>
             </div>
         </div>
     );
-    
 }
--- src/app/HomeMaga/Transporte/page.jsx
+++ src/app/HomeMaga/Transporte/page.jsx
@@ -132,11 +132,15 @@ function TransporteHome() {
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Apellido</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Dirección</th>
+              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Esquina</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Teléfono</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Mascota</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Foto</th>
+              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Canil</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Pago</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Estado</th>
               <th>Próximo estado</th>
             </tr>
@@ -148,8 +152,12 @@ function TransporteHome() {
                   <td className="px-6 py-4 whitespace-nowrap">{turno.nombre}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{turno.apellido}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{turno.direccion}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{turno.esquina}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{turno.telefono}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{turno.selectedPet}</td>
+                  <td className="px-6 py-4 whitespace-nowrap"><Image src={turno.foto} alt="canil" width={50} height={50} className='rounded-full' /></td>
+                  <td className="px-6 py-4 whitespace-nowrap">{turno.canilPeluqueria}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{turno.pago ? "Si" : "Cobrar"} : {turno.precio}</td>
                   {/*Renderizado del estado Actual*/}
                   {turno.estadoDelTurno === "confirmado" ?
                   <td className="px-6 py-4 whitespace-nowrap">Buscar</td>
