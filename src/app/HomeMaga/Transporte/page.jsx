7df9ce0e011449464ab1e4206798152f10998a0a
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2024-05-24 10:21:29 -0300

    cagadas de viernes por la mañana

--- src/app/HomeMaga/Transporte/page.jsx
+++ src/app/HomeMaga/Transporte/page.jsx
@@ -223,7 +223,7 @@ export default function TransporteHome() {
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Dirección</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Teléfono</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Estado</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Cobrar</th>                                         '
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Cobrar</th>      
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
             </tr>
           </thead>
