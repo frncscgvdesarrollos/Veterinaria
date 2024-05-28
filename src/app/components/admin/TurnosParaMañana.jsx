3bd6834a68413dc7627f212abedfc297c65a567b
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2024-05-28 20:40:53 -0300

    cagadas de martes por la noche

--- src/app/components/admin/TurnosParaMañana.jsx
+++ src/app/components/admin/TurnosParaMañana.jsx
@@ -63,7 +63,7 @@ export default function TurnosParaMañana() {
             }
             getTurnos();
             handleTurnos();
-        }, []);
+        }, [verMasTurnos]);
         
 
     return (
