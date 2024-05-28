9105b20611d88cefc4da3cbf3e6d5ca87edb0890
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2024-05-28 20:45:06 -0300

    cagadas de martes por la noche

--- src/app/components/admin/llamarA.jsx
+++ src/app/components/admin/llamarA.jsx
@@ -42,26 +42,56 @@ export default function LlamarA() {
     <div className="container mx-auto px-4">
       <h1 className="text-3xl font-bold mb-4 text-center">Turnos para mañana</h1>
       <div className="grid gap-4">
-        {turnosParaHoy.map(turno => (
-          <div key={turno.id} className="bg-violet-300 rounded-lg p-4">
-            <div className="flex flex-col md:flex-row md:justify-between md:gap-4">
-              <div className="flex gap-2 md:flex-row md:gap-2 text-purple-800">
-                <p className="text-lg mb-2 md:text-xl">{turno.nombre}</p>
-                <p className="text-lg md:text-xl">{turno.telefono}</p>
-              </div>
-              <div className="flex gap-2 md:flex-row md:gap-2 text-purple-800">
-                <p className="text-lg md:text-xl">{turno.selectedPet}</p>
-                <p className="text-lg md:text-xl">{turno.selectedTurno}</p>
-                <p className="text-lg md:text-xl">{turno.precio}</p>
-              </div>
-              <div className="flex gap-2 md:gap-4 ml-auto">
-                <button onClick={() => confirmarTurno(turno.id)} className="bg-blue-600 text-white rounded-lg py-2 px-4 md:py-3 md:px-6 text-base md:text-lg">Confirmar</button>
-                <button onClick={() => cancelarTurno(turno.id)} className="bg-red-600 text-white rounded-lg py-2 px-4 md:py-3 md:px-6 text-base md:text-lg">Cancelar</button>
-              </div>
+  <div>
+    <h2 className="text-2xl font-bold mb-2">Turnos para la mañana:</h2>
+    {turnosParaHoy
+      .filter(turno => turno.selectedTurno === "mañana")
+      .map((turno, index) => (
+        <div key={turno.id} className="bg-violet-300 rounded-lg p-4">
+          <div className="flex flex-col md:flex-row md:justify-between md:gap-4">
+            <div className="flex gap-2 md:flex-row md:gap-2 text-purple-800">
+              <p className="text-lg mb-2 md:text-xl">{`${index + 1}. ${turno.nombre}`}</p>
+              <p className="text-lg md:text-xl">{turno.telefono}</p>
+            </div>
+            <div className="flex gap-2 md:flex-row md:gap-2 text-purple-800">
+              <p className="text-lg md:text-xl">{turno.selectedPet}</p>
+              <p className="text-lg md:text-xl">{turno.selectedTurno}</p>
+              <p className="text-lg md:text-xl">{turno.precio}</p>
+            </div>
+            <div className="flex gap-2 md:gap-4 ml-auto">
+              <button onClick={() => confirmarTurno(turno.id)} className="bg-blue-600 text-white rounded-lg py-2 px-4 md:py-3 md:px-6 text-base md:text-lg">Confirmar</button>
+              <button onClick={() => cancelarTurno(turno.id)} className="bg-red-600 text-white rounded-lg py-2 px-4 md:py-3 md:px-6 text-base md:text-lg">Cancelar</button>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
+  </div>
+  <div>
+    <h2 className="text-2xl font-bold mb-2">Turnos para la tarde:</h2>
+    {turnosParaHoy
+      .filter(turno => turno.selectedTurno === "tarde")
+      .map((turno, index) => (
+        <div key={turno.id} className="bg-violet-300 rounded-lg p-4">
+          <div className="flex flex-col md:flex-row md:justify-between md:gap-4">
+            <div className="flex gap-2 md:flex-row md:gap-2 text-purple-800">
+              <p className="text-lg mb-2 md:text-xl">{`${index + 1}. ${turno.nombre}`}</p>
+              <p className="text-lg md:text-xl">{turno.telefono}</p>
+            </div>
+            <div className="flex gap-2 md:flex-row md:gap-2 text-purple-800">
+              <p className="text-lg md:text-xl">{turno.selectedPet}</p>
+              <p className="text-lg md:text-xl">{turno.selectedTurno}</p>
+              <p className="text-lg md:text-xl">{turno.precio}</p>
+            </div>
+            <div className="flex gap-2 md:gap-4 ml-auto">
+              <button onClick={() => confirmarTurno(turno.id)} className="bg-blue-600 text-white rounded-lg py-2 px-4 md:py-3 md:px-6 text-base md:text-lg">Confirmar</button>
+              <button onClick={() => cancelarTurno(turno.id)} className="bg-red-600 text-white rounded-lg py-2 px-4 md:py-3 md:px-6 text-base md:text-lg">Cancelar</button>
+            </div>
+          </div>
+        </div>
+      ))}
+  </div>
+</div>
+
     </div>
   );
 }
